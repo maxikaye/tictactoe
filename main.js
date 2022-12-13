051dbbe618eacf1217fcc3c30bd4a69a23b2136a
Maxi Kaye <mr.max.kaye@gmail.com>
2022-12-13 15:07:42 -0500

    Refactor gameBoard module

--- main.js
+++ main.js
@@ -3,74 +3,79 @@ const gameCtrl = (() => {
     // DOM Cache
     const gameArea = document.querySelector('.game-area');
 
-    let round = 0;
     // game state
+    let round = 0;
 
-    return { gameArea }
+    return { gameArea, round }
 })();
 
 
 const gameBoard = (() => {
-    // let _board = [['', '', ''], ['', '', ''], ['', '', '']];
-    let _board = [['x', 'o', 'o'], ['o', 'x', 'x'], ['x', 'o', 'x']];
+    let _board = ['', '', '', '', '', '', '', '', ''];
+    // DOM Cache
     const boardDom = document.createElement('div');
     boardDom.classList.add('game-board')
     gameCtrl.gameArea.appendChild(boardDom);
 
+    const setPlayerMark = (e) => {
+        gameCtrl.round += 1;
+        const space = Number(e.target.id);
+        _board[space] = 'x';
+        e.target.innerHTML = 'X';
+    }
+
     const render = () => {
-        for (const row of _board) {
-            for (const space of row) {
-                // create div with id and class and append
+        for (let i = 0; i < _board.length; i++) {
                 let boardSpace = document.createElement('div');
                 boardSpace.classList.add('board-space');
-                boardSpace.innerHTML = space.toUpperCase();
+                boardSpace.id = i;
                 boardDom.appendChild(boardSpace);
-            }
+                boardSpace.addEventListener('click', setPlayerMark);
         }
     };
-    // if all game board spaces are filled
+
     const checkWin = () => {
-        if (_board[0][0] && _board[0][1] === _board[0][2]) {
-            return _board[0][0]; // first row
-        } else if (_board[1][0] && _board[1][1] === _board[1][2]) {
-            return _board[1][0]; // second row
-        } else if (_board[2][0] && _board[2][1] === _board[2][2]) {
-            return _board[2][0]; // third row
-        } else if (_board[0][0] && _board[1][0] === _board[2][0]) {
-            return _board[0][0]; // first col
-        } else if (_board[0][1] && _board[1][1] === _board[2][1]) {
-            return _board[0][1]; // second col
-        } else if (_board[0][2] && _board[1][2] === _board[2][2]) {
-            return _board[0][2]; // third col
-        } else if (_board[0][0] && _board[1][1] === _board[2][2]) {
-            return _board[0][0]; // top left to bottom right diagonal
-        } else if (_board[0][2] && _board[1][1] === _board[2][0]) {
-            return _board[0][1]; // top right to bottom left diagonal
+        if (_board[0] && _board[1] === _board[2]) {
+            return _board[0]; // first row
+        } else if (_board[3] && _board[4] === _board[5]) {
+            return _board[3]; // second row
+        } else if (_board[6] && _board[7] === _board[8]) {
+            return _board[6]; // third row
+        } else if (_board[0] && _board[3] === _board[6]) {
+            return _board[0]; // first col
+        } else if (_board[1] && _board[4] === _board[7]) {
+            return _board[1]; // second col
+        } else if (_board[2] && _board[5] === _board[8]) {
+            return _board[2]; // third col
+        } else if (_board[0] && _board[4] === _board[8]) {
+            return _board[0]; // top left to bottom right diagonal
+        } else if (_board[2] && _board[4] === _board[6]) {
+            return _board[2]; // top right to bottom left diagonal
         } else {
-            return "tie";
+            return "draw";
         }
     };
     return { render, checkWin };
 })();
 
 
-const Events = (() => {
-    let _events = {};
+// const Events = (() => {
+//     let _events = {};
     
-    const listen = (event, callback) => {
+//     const listen = (event, callback) => {
 
-    };
+//     };
 
-    const cancel = (event) => {
+//     const cancel = (event) => {
 
-    };
+//     };
 
-    const emit = (event, ...data) => {
+//     const emit = (event, ...data) => {
 
-    };
+//     };
 
-    return { listen, cancel, emit };
-})();
+//     return { listen, cancel, emit };
+// })();
 
 
 const Player = () => {
